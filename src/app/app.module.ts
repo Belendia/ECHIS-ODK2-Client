8f6fc00f76e7b8cbdbd61b4e743d5c4d80a84bb3
Belendia Abdissa <belendia@gmail.com>
2018-01-24 22:30:49 +0300

    Add MatButtonModule to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule } from '@angular/material';
+import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HamletsComponent } from './village-profile/hamlets/hamlets.component';
@@ -20,6 +20,7 @@ import { AppRoutingModule } from './app-routing.module';
     MatIconModule,
     MatSidenavModule,
     MatListModule,
+    MatButtonModule,
     AppRoutingModule
   ],
   providers: [],
